11c073d0bc245d7dd739567e5c0f1a736b6ed366
Leonardo Facundes L3onix <leonardo.fm00@gmail.com>
2024-03-06 18:19:08 -0300

    create Sale factory

---
+++ database/factories/SaleFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Sale>
+ */
+class SaleFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'sales_id' => fake()->unique()->regexify('[0-9]{9}'),
+            'total_price' => fake()->randomFloat(2)
+        ];
+    }
+}
