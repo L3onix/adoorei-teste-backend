873ce70c16c2f99c0831aaeba134ddeea7c58a81
Leonardo Facundes L3onix <leonardo.fm00@gmail.com>
2024-03-06 18:20:53 -0300

    create Sale migration with product_sale table

---
+++ database/migrations/2024_03_05_043844_create_sales_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('sales', function (Blueprint $table) {
+            $table->id();
+            $table->string('sales_id')->unique();
+            $table->float('total_price');
+            $table->timestamps();
+        });
+        Schema::create('product_sale', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('sale_id')->constrained('sales');
+            $table->foreignId('product_id')->constrained('products');
+            $table->integer('amount')->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('product_sale');
+        Schema::dropIfExists('sales');
+    }
+};
