a6428f4183c06718ad0c4aa01bcd6e77aadf3f6f
Leonardo Facundes L3onix <leonardo.fm00@gmail.com>
2024-03-06 23:26:43 -0300

    add index Product route

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\ProductCollection;
+use App\Http\Resources\ProductResource;
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function index()
+    {
+        $products = Product::all();
+        return ProductResource::collection($products);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +17,27 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+// lista de produtos disponiveis
+Route::get('/product', [ProductController::class, 'index']);
+// cadastrar nova venda
+Route::post('/sale', function (Request $request) {
+    return response();
+});
+// consultar vendas realizadas
+Route::get('/sale', function (Request $request) {
+    return response();
+});
+// consultar uma venda específica
+Route::get('/sale/{id}', function (Request $request) {
+    return response();
+});
+// cancelar uma venda
+Route::delete('/sale/{id}', function (Request $request) {
+    return response();
+});
+// cadastrar produtos a uma venda
+Route::put('/sale/{id}', function (Request $request) {
+    return response();
 });
