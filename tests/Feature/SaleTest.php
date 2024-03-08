cab2a440699ffe97c0a2731e12dbc20a0fb78857
Leonardo Facundes L3onix <leonardo.fm00@gmail.com>
2024-03-08 04:54:39 -0300

    create setUp method and get endpoint test

--- tests/Feature/SaleTest.php
+++ tests/Feature/SaleTest.php
@@ -10,9 +10,14 @@ class SaleTest extends TestCase
 {
     use RefreshDatabase;
 
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->seed();
+    }
+
     public function test_sale_post_endpoint(): void
     {
-        $this->seed();
         $json = '{
             "sales_id": "2023010134",
             "products": [
@@ -33,7 +38,6 @@ class SaleTest extends TestCase
 
     public function test_sale_post_endpoint_invalid_input(): void
     {
-        $this->seed();
         $json = '{
             "sales_id": "2023010134",
             "products": [
@@ -50,5 +54,34 @@ class SaleTest extends TestCase
         $response = $this->postJson(route('sales.store'), json_decode($json, true));
 
         $response->assertStatus(422);
+    }
+
+    public function test_sale_get_endpoint(): void
+    {
+        $json = '{
+            "data": [
+                {
+                    "sales_id": "202301011",
+                    "amount": 8200,
+                    "products": [
+                        {
+                            "id": 1,
+                            "name": "Celular 1",
+                            "price": 1800,
+                            "amount": 1
+                        },
+                        {
+                            "id": 2,
+                            "name": "Celular 2",
+                            "price": 3200,
+                            "amount": 2
+                        }
+                    ]
+                }
+            ]
+        }';
+        $response = $this->getJson(route('sales.index'));
+        $response->assertStatus(200)
+            ->assertExactJson(json_decode($json, true));
     }
 }
